d07181c637fe57d221eef6a1a6404fb585db6383
Aaron Toys <aarontoys@gmail.com>
2016-03-28 13:33:54 -0600

    add sidebar to books & authors pages

--- src/server/routes/books.js
+++ src/server/routes/books.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Galvanize Reads', subtitle: 'Books' });
+  res.render('books', { title: 'Galvanize Reads', subtitle: 'Books' });
 });
 
 module.exports = router;
